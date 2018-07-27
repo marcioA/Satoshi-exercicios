23539fa92ad0260abd8b3859b07b534a656461aa
marcioA <zamar_pinheiro@hotmail.com>
2018-07-27 00:12:14 -0300

    subindo arquivos novos nivel very easy

---
+++ areaQuad.py
@@ -0,0 +1,5 @@
+lado = int(input("Digite o valor para o lado do quadrado: "))
+
+area = lado*lado
+
+print(area)
---
+++ reajusteSalario.py
@@ -0,0 +1,5 @@
+salario = float(input("Digite o valor do salario"))
+
+salario = salario*1.15
+
+print(salario)
